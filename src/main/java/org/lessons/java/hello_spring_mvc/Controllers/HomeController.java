09f19648e22a9461a0390993d0ba6cc04d526fc9
gianmarcodalfonso <gianmarcoda10.gg@gmail.com>
2026-01-12 16:09:23 +0100

    first commit

---
+++ src/main/java/org/lessons/java/hello_spring_mvc/Controllers/HomeController.java
@@ -0,0 +1,10 @@
+package org.lessons.java.hello_spring_mvc.Controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/")
+public class HomeController {
+
+}
