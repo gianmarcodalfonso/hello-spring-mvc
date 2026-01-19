966147599cbe17d6480fa3d820957f809ccf3799
gianmarcodalfonso <gianmarcoda10.gg@gmail.com>
2026-01-19 10:26:15 +0100

    Update HomeController and Home.html template for initial setup

--- src/main/java/org/lessons/java/hello_spring_mvc/Controllers/HomeController.java
+++ src/main/java/org/lessons/java/hello_spring_mvc/Controllers/HomeController.java
@@ -2,9 +2,14 @@ package org.lessons.java.hello_spring_mvc.Controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 @RequestMapping("/")
 public class HomeController {
-
+  
+  @GetMapping("/")
+  public String home(){
+    return "Home";
+  }
 }
