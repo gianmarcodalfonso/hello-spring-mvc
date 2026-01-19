f4e5161a4aed7de378e3e07e0f87cc9e38ab4545
gianmarcodalfonso <gianmarcoda10.gg@gmail.com>
2026-01-19 11:06:29 +0100

    Add greetings endpoint and corresponding Thymeleaf template

--- src/main/java/org/lessons/java/hello_spring_mvc/Controllers/HomeController.java
+++ src/main/java/org/lessons/java/hello_spring_mvc/Controllers/HomeController.java
@@ -1,7 +1,11 @@
 package org.lessons.java.hello_spring_mvc.Controllers;
 
+import java.time.LocalTime;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
@@ -12,4 +16,14 @@ public class HomeController {
   public String home(){
     return "Home";
   }
+
+  @GetMapping("/greetings")
+  public String Welcome(@RequestParam(name = "name") String name, Model model){
+    model.addAttribute("email", "ekqb@gmail.com");
+    model.addAttribute("currentDate", LocalTime.now());
+    model.addAttribute("name", name);
+
+    return "greetings";
+  }
+
 }
